0c0a000f12c3ba27418de0b912dfdc5472fae401
Shreya Kumar <sk2329@cornell.edu>
2024-05-16 22:26:49 -0400

    updates

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function goToPage(page) {
   window.location.href = page; // Redirects to the given page within your website
 }
+
+function openInNewTab(url) {
+  window.open(url, '_blank'); // Opens the given URL in a new tab
+}
